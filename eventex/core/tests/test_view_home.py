e86476de278a87a3647bbad9b6923d22c6d20be6
gustavo7lagoas <gustavo7lagoas@gmail.com>
2018-02-12 06:45:51 +0000

    Adiciona keynote speakers

--- eventex/core/tests/test_view_home.py
+++ eventex/core/tests/test_view_home.py
@@ -17,3 +17,33 @@ class HomeTest(TestCase):
     def test_subscription_link(self):
         expected = 'href="{}"'.format(r('subscriptions:new'))
         self.assertContains(self.response, expected)
+
+    def test_speakers(self):
+        """ Must show keynote speakers """
+        contents = [
+            "Grace Hopper",
+            "http://hbn.link/hopper-pic",
+            "Alan Turing",
+            "http://hbn.link/turing-pic"
+        ]
+        for expect in contents:
+            with self.subTest():
+                self.assertContains(self.response, expect)
+
+    def test_speakers_link(self):
+        """ Must contain link to speakers section """
+        expected = 'href="{}#speakers"'.format(r('home'))
+        self.assertContains(self.response, expected)
+
+    def test_nav_items(self):
+        """ Must check navegation menu """
+        navegation_menu = [
+            'href="{}#overview"'.format(r('home')),
+            'href="{}#speakers"'.format(r('home')),
+            'href="{}#sponsors"'.format(r('home')),
+            'href="{}#register"'.format(r('home')),
+            'href="{}#venue"'.format(r('home'))
+        ]
+        for menu_item in navegation_menu:
+            with self.subTest():
+                self.assertContains(self.response, menu_item)
